3add60245bbd2202b42635f0c05c18d3e35dfe3c
kitadesega <segarian8@gmail.com>
2019-08-04 17:21:16 +0900

    まぁ色々あるんだよ

---
+++ pages/Tabitem.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- <button @click="$emit('input', id)" :class="[active, 'tab']"> -->
+  <button @click="$emit('yato')" :class="[active, 'tab']">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    label: String,
+    value: Number
+  },
+  computed: {
+    active() {
+      return this.value === this.id ? 'active' : false
+    }
+  }, 
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.tab {
+  border-radius: 2px 2px 0 0;
+  background: #fff;
+  color: #311d0a;
+  line-height: 24px;
+}
+.tab:hover {
+  background: #eeeeee;
+}
+.active {
+  background: #f7c9c9;
+}
+</style>
--- pages/comment.vue
+++ pages/comment.vue
@@ -2,7 +2,7 @@
   <v-layout>
     <v-flex xs12 sm6 offset-sm3>
       <div style="padding-bottom:60px"><v-card>
-      <v-toolbar fixed light height="">
+      <v-toolbar fixed light>
         <v-btn icon>
           <nuxt-link :to="{path: '/item_detail', query: {itemId: itemId }}">
           <v-icon>arrow_back</v-icon>
@@ -13,23 +13,44 @@
       </v-toolbar>
       </v-card>
     </div>
-    <div class="line__container">
-      <div class="line__contents scroll" id ="chatarea">
+    <div class="line__container" style="height:auto">
+      <div class="line__contents scroll" id ="chatarea" style="height:auto">
+        <v-list three-line>
+            <v-list-tile
+                 v-for="(value, index) in chat" :key="index"
+                avatar
+                style="height:auto;"
+            >
+              <v-list-tile-avatar>
+                <img :src="value.userPhoto">
+              </v-list-tile-avatar>
+
+              <v-list-tile-content style="height:auto;">
+                <v-list-tile-sub-title style="margin:0 0 10px 0;">{{value.userName}}</v-list-tile-sub-title>
+                <v-list-tile-sub-title class="text--primary subheading" style="margin:0 0 5px 0;">{{value.chatMessage}}</v-list-tile-sub-title>
+                <v-list-tile-sub-title style="height:auto">
+                  {{value.createdAt.toDate().toLocaleString()}}
+                </v-list-tile-sub-title>
+              </v-list-tile-content>
+
+            </v-list-tile>
+        </v-list>
       </div>
     </div>
-    <v-container style="margin-top:20%">
-      <v-flex xs11 style="width:100%;display:inline-block ">
-      <v-text-field
-        v-model="comment"
-        :rules="nameRules"
-        label=""
-        required
-      ></v-text-field>
-      </v-flex>
-      <v-flex xs1 style="display:inline-block" >
-        <v-icon>send</v-icon>
-      </v-flex>
-    </v-container>
+    <v-card height="60px" flat > 
+      <v-footer height="80px" :fixed=true>
+          <v-flex xs10 style="padding:0 0 0 20px;width:50%;display:inline-block ">
+          <v-text-field
+            v-model="input"
+            label=""
+            required
+          ></v-text-field>
+          </v-flex>
+          <v-flex xs2 style="display:inline-block" >
+              <v-icon @click.prevent="sendChat(input)">send</v-icon>
+          </v-flex>
+      </v-footer>
+    </v-card> 
   </v-flex>
 </v-layout>
   
@@ -70,37 +91,54 @@ import '@/assets/css/chat.css'
     
   },
     methods : {
-      ...mapActions(['setUser']), 
-      sendMessage(){
-        firebase.auth().onAuthStateChanged(user => {
+      ...mapActions(['setUser']),
+      sendChat: function (data) {
+        this.input = "";
+        if(data){
+          firebase.auth().onAuthStateChanged(user => {
             this.user = user ? user : {}
-            //console.log(this.user.uid)
+            console.log(data);
             const db = firebase.firestore()
-            // ストレージオブジェクト作成
-            var storageRef = firebase.storage().ref();
-            //ファイル名が被らないように一意な名前を作成
-            this.imageName = uuid();
-            // ファイルのパスを設定
-            var mountainsRef = storageRef.child(`images/${this.imageName}`);
-            
-        })
-      },
+            db.collection("item").doc(this.itemId).collection("chat").doc().set({
+              user_id: user.uid,
+              user_photo: user.photoURL,
+              user_name: user.displayName,
+              chat_message: data,
+              display_flg: true,
+              created_at: new Date(),
+            })
+          })
+        } 
+      }, 
       ichiran(){
         this.chat=[];
-            const db = firebase.firestore()
-             var docRef = db.collection("item").doc(this.itemId).collection("comment");
-               docRef.get().then(querySnapshot => {
-            querySnapshot.forEach(doc =>{
-              // doc.data() is never undefined for query doc snapshots
-              console.log(doc.id, " => ", doc.data());
-              console.log(this.exChang);
-              //ループでとる場合 * JSの場合
-              // this.exChang.forEach(element => {
-              //   console.log(element.comment);
-                this.chat.push(doc.data().comment);
-              // });
-            });
-          });
+        firebase.auth().onAuthStateChanged(user => {
+          // User is signed in.
+          //userにログインしているユーザーのデータを入れる
+          this.user = user ? user : {}
+          //firestore設定
+          const db = firebase.firestore()
+          //itemコレクションを選択
+          var docRef = db.collection("item").doc(this.itemId)
+            .collection("chat").orderBy("created_at", "asc");
+          //データ取得の条件を指定して取得
+
+            //データ取得
+          docRef.onSnapshot(snapshot => {
+              snapshot.docChanges().forEach(chat => {
+                  let data = {
+                  'userId': chat.doc.user_id,
+                  'userPhoto': chat.doc.data().user_photo,
+                  'userName': chat.doc.data().user_name,
+                  'chatMessage': chat.doc.data().chat_message,
+                  'displayFlg': chat.doc.data().display_flg,
+                  'createdAt': chat.doc.data().created_at,
+                }
+                console.log(data)
+                this.chat.push(data);
+              });
+          })
+        })
       }
     },
   
@@ -117,6 +155,5 @@ import '@/assets/css/chat.css'
 #comments{
   text-align: left;
 }
-
 
 </style>
